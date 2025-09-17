ae6f8e0ca48ccaae7db78545bad2867088a0a79b
molor824 <molormargaderdene@gmail.com>
2025-09-17 11:58:54 +0800

    First commit

---
+++ __main__.py
@@ -0,0 +1,84 @@
+import pygame
+import raytracing
+import multiprocessing as mp
+import multiprocessing.shared_memory as shm
+import pickle
+import struct
+import numpy as np
+
+WIDTH = 800
+HEIGHT = 600
+
+BUF_WIDTH = WIDTH
+BUF_HEIGHT = HEIGHT
+
+FPS = 60
+DELTA = 1.0 / FPS
+
+OBJ_MEM_CAPACITY = 1024 * 1024
+
+def write_buffer(buffer, value):
+    pickled = pickle.dumps(value)
+    buffer[0:4] = struct.pack("i", len(pickled))
+    buffer[4:len(pickled) + 4] = pickled
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+
+    pixel_mem = shm.SharedMemory(create=True, size=BUF_WIDTH * BUF_HEIGHT * 3)
+
+    params_mem = shm.SharedMemory(create=True, size=OBJ_MEM_CAPACITY)
+    params = raytracing.Parameters(np.array((0.0, 0.0, 0.0)))
+    params.objects.append(raytracing.Circle(np.array((0.0, 0.0, 200.0)), 50.0))
+    write_buffer(params_mem.buf, params)
+
+    quit_event = mp.Event()
+
+    cpu_count = mp.cpu_count() - 1
+    processes = [mp.Process(
+        target=raytracing.raytrace,
+        args=(np.array((BUF_WIDTH, BUF_HEIGHT)), cpu_count, i, pixel_mem.name, params_mem.name, quit_event)
+    ) for i in range(cpu_count)]
+
+    for process in processes:
+        process.start()
+
+    running = True
+    while running:
+        clock.tick(FPS)
+
+        keys = pygame.key.get_pressed()
+        direction = [0.0, 0.0, 0.0]
+        if keys[pygame.K_RIGHT]:
+            direction[0] += 1.0
+        if keys[pygame.K_LEFT]:
+            direction[0] -= 1.0
+        if keys[pygame.K_UP]:
+            direction[1] -= 1.0
+        if keys[pygame.K_DOWN]:
+            direction[1] += 1.0
+        
+        params.position = params.position + np.array(direction) * (DELTA * 40.0)
+        write_buffer(params_mem.buf, params)
+
+        array = np.resize(np.frombuffer(pixel_mem.buf, dtype=np.uint8), (BUF_WIDTH, BUF_HEIGHT, 3))
+
+        pygame.surfarray.blit_array(screen, array)
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    quit_event.set()
+
+    for process in processes:
+        process.join()
+
+    pixel_mem.unlink()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
---
+++ raytracing.py
@@ -0,0 +1,69 @@
+import multiprocessing.shared_memory as shm
+import struct
+import pickle
+import numpy as np
+import math
+
+class Object:
+    def __init__(self, position: np.ndarray):
+        self.position = position
+
+class RayIntersection:
+    def __init__(self, distance: float, normal: np.ndarray):
+        self.distance = distance
+        self.normal = normal
+
+class Circle(Object):
+    def __init__(self, position: np.ndarray, radius: float):
+        super().__init__(position)
+        self.radius = radius
+    def intersect(self, origin: np.ndarray, direction: np.ndarray):
+        direction = direction / np.linalg.norm(direction)
+        offset_position = self.position - origin
+        projected_distance = np.dot(offset_position, direction)
+        projected_point = origin + direction * projected_distance
+        closest_distance = np.linalg.norm(projected_point - self.position)
+
+        if closest_distance <= self.radius:
+            hit_dist_from_circle = math.sqrt(self.radius * self.radius - closest_distance * closest_distance)
+            hit_dist = projected_distance - hit_dist_from_circle
+            hit_point = origin + direction * hit_dist
+            normal = (hit_point - self.position) / self.radius
+            return RayIntersection(hit_dist, normal)
+
+class Parameters:
+    def __init__(self, position: np.ndarray, fov = 1.0):
+        self.position = position
+        self.fov = fov
+        self.objects: list[Object] = []
+
+class Move:
+    def __init__(self, direction: np.ndarray):
+        self.direction = direction
+
+def load_buffer(buffer):
+    size = struct.unpack("i", buffer[0:4])[0]
+    return pickle.loads(buffer[4:size + 4])
+
+def raytrace(size: np.ndarray, process_count: int, process_index: int, pixel_buffer_name: str, params_buffer_name: str, quit_event):
+    pixel_mem = shm.SharedMemory(pixel_buffer_name)
+    params_mem = shm.SharedMemory(params_buffer_name)
+
+    center = size / 2
+    
+    while True:
+        for y in range(size[1]):
+            for x in range(process_index, size[0], process_count):
+                if quit_event.is_set(): return
+
+                params: Parameters = load_buffer(params_mem.buf)
+                origin = params.position
+                direction = np.array((x + 0.5 - center[0], (y + 0.5 - center[1]) * params.fov, size[1]), dtype=np.float64)
+                start = (x * size[1] + y) * 3
+
+                result = min((obj.intersect(origin, direction) for obj in params.objects), key=lambda hit: hit.distance if hit != None else math.inf)
+                
+                if result != None:
+                    pixel_mem.buf[start:start+3] = np.astype(result.normal * 127 + 127, np.uint8).tobytes()
+                else:
+                    pixel_mem.buf[start:start+3] = bytes((0, 0, 0))
